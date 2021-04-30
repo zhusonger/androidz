e3e4c57c0dbdc1fc4cc11e39c1988729aa897969
zhusong <song.zhu@lasong.com.cn>
2021-04-30 15:21:15 +0800

    添加设置控件点击透明度的事件

--- app/src/main/java/cn/com/lasong/widget/WidgetActivity.java
+++ app/src/main/java/cn/com/lasong/widget/WidgetActivity.java
@@ -9,6 +9,7 @@ import cn.com.lasong.R;
 import cn.com.lasong.app.AppBaseActivity;
 import cn.com.lasong.databinding.ActivityWidgetBinding;
 import cn.com.lasong.widget.dialog.TopSheetDialog;
+import cn.com.lasong.widget.utils.ViewHelper;
 
 /**
  * Author: zhusong
@@ -19,29 +20,20 @@ import cn.com.lasong.widget.dialog.TopSheetDialog;
 public class WidgetActivity extends AppBaseActivity {
 
     private ActivityWidgetBinding binding;
+    private TopSheetDialog dialog;
+    private IconMoveView moveView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityWidgetBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        binding.tvText.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                if (binding.tvText.getVisibility() == View.VISIBLE) {
-                    binding.tvText.setVisibility(View.GONE);
-                } else {
-                    binding.tvText.setVisibility(View.VISIBLE);
-                }
-
-                binding.tvText.postDelayed(this, 2000);
-            }
-        }, 2000);
-    }
-
-    @Override
-    protected int fitStatusBarResId() {
-        return R.id.ll_content;
+        binding.btnTopSheet.setOnClickListener(this::onClick);
+        binding.btnMove.setOnClickListener(this::onClick);
+//        binding.llShadow.setOnClickListener(this::onClick);
+        binding.shadowLayout.setOnClickListener(this::onClick);
+        ViewHelper.setClickAlpha(binding.shadowLayout);
+        ViewHelper.setClickAlpha(binding.btnMove);
     }
 
     @Override
@@ -49,41 +41,37 @@ public class WidgetActivity extends AppBaseActivity {
         return FitStatusBarType.MARGIN;
     }
 
-    private TopSheetDialog dialog;
-    private IconMoveView moveView;
-    public void onClick(View view) {
-        int id = view.getId();
-        switch (id) {
-            case R.id.btn_top_sheet: {
-                if (null != dialog)
-                    dialog.dismiss();
-                dialog = new TopSheetDialog(this);
-                dialog.setContentView(R.layout.view_lyric);
-                // 可隐藏
-                dialog.setCancelable(true);
-                // 是否拦截触摸事件
-                dialog.setConsumeTouch(false);
-                // 在控件外触摸不隐藏
-                dialog.setCanceledOnTouchOutside(false);
-                dialog.show();
-                break;
-            }
+    @Override
+    protected int fitStatusBarResId() {
+        return R.id.ll_content;
+    }
 
-            case R.id.btn_move: {
-                if (null != moveView) {
-                    if (moveView.getVisibility() == View.VISIBLE) {
-                        moveView.hide();
-                        binding.btnMove.setText("显示浮窗");
-                    } else {
-                        moveView.show(this);
-                        binding.btnMove.setText("隐藏浮窗");
-                    }
+    public void onClick(View view) {
+        if (view == binding.btnTopSheet) {
+            if (null != dialog)
+                dialog.dismiss();
+            dialog = new TopSheetDialog(this);
+            dialog.setContentView(R.layout.view_lyric);
+            // 可隐藏
+            dialog.setCancelable(true);
+            // 是否拦截触摸事件
+            dialog.setConsumeTouch(false);
+            // 在控件外触摸不隐藏
+            dialog.setCanceledOnTouchOutside(false);
+            dialog.show();
+        } else if (view == binding.btnMove) {
+            if (null != moveView) {
+                if (moveView.getVisibility() == View.VISIBLE) {
+                    moveView.hide();
+                    binding.btnMove.setText("显示浮窗");
                 } else {
-                    moveView = new IconMoveView(this);
                     moveView.show(this);
                     binding.btnMove.setText("隐藏浮窗");
                 }
-                break;
+            } else {
+                moveView = new IconMoveView(this);
+                moveView.show(this);
+                binding.btnMove.setText("隐藏浮窗");
             }
         }
     }
