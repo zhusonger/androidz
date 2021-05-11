6b7805c20e546ce88c26e1dbe1a30d6422109e5f
zhusong <song.zhu@lasong.com.cn>
2021-05-11 19:03:49 +0800

    添加索引控件

--- app/src/main/java/cn/com/lasong/widget/StickHeaderActivity.java
+++ app/src/main/java/cn/com/lasong/widget/StickHeaderActivity.java
@@ -1,8 +1,12 @@
 package cn.com.lasong.widget;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,6 +25,7 @@ import cn.com.lasong.widget.adapterview.itemdecoration.StickHeaderProvider;
 import cn.com.lasong.widget.adapterview.itemdecoration.StickyHeaderDecoration;
 import cn.com.lasong.widget.adapterview.adapter.OnItemClickListener;
 import cn.com.lasong.widget.adapterview.adapter.ZRecyclerViewAdapter;
+import cn.com.lasong.widget.any.AnyIndexProvider;
 
 /**
  * Author: zhusong
@@ -33,19 +38,44 @@ public class StickHeaderActivity extends AppBaseActivity implements StickHeaderP
     ActivityStickHeaderBinding binding;
     ZRecyclerViewAdapter<String> adapter;
     List<String> data = new ArrayList<>();
+    List<String> group = new ArrayList<>();
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityStickHeaderBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.viewIndex.setProvider(new AnyIndexProvider() {
+            @Override
+            public int indexCount() {
+                return group.size();
+            }
+
+            @Override
+            public View inflateIndex(LayoutInflater inflater, AnyIndexView parent, int position) {
+                TextView textView = new TextView(parent.getContext());
+                textView.setText(group.get(position));
+                textView.setGravity(Gravity.CENTER_HORIZONTAL);
+                textView.setPadding(20, 20, 20, 20);
+                textView.setBackgroundColor(Color.YELLOW);
+                textView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.e("Test", "点击索引位置:" + position+", text:" + textView.getText());
+                    }
+                });
+                return textView;
+            }
+        });
         for (int i = 0; i < 100; i++) {
             if (i % 10 == 0) {
                 data.add("Group"+i);
+                group.add("G"+i);
             } else {
                 data.add("Text"+i);
             }
         }
+        binding.viewIndex.updateIndex();
         adapter = new ZRecyclerViewAdapter<String>(data, R.layout.item_sticker_header, new OnItemClickListener() {
             @Override
             public void onItemClick(View view, int position) {
