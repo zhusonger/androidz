c4dcc86cc98cb449899b8b98ebd76bb8563fbcf0
zhusong <song.zhu@lasong.com.cn>
2021-06-07 21:12:20 +0800

    首页结构

--- zapp/src/main/java/cn/com/lasong/zapp/ui/video/VideoViewModel.kt
+++ zapp/src/main/java/cn/com/lasong/zapp/ui/video/VideoViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class VideoViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "This is Video Fragment"
     }
     val text: LiveData<String> = _text
 }
