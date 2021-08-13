522596846706e4a88c0080e332f5bb0a5770b986
zhusong <song.zhu@lasong.com.cn>
2021-08-13 11:05:23 +0800

    添加合成与剪辑的测试代码

--- app/src/main/java/cn/com/lasong/mux/MuxActivity.java
+++ app/src/main/java/cn/com/lasong/mux/MuxActivity.java
@@ -9,12 +9,17 @@ import android.view.View;
 
 import androidx.annotation.Nullable;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.InputStream;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import cn.com.lasong.app.AppBaseActivity;
 import cn.com.lasong.base.result.PERCallback;
 import cn.com.lasong.databinding.ActivityMuxBinding;
+import cn.com.lasong.media.AVPixelFormat;
 import cn.com.lasong.media.Muxer;
 import cn.com.lasong.utils.ILog;
 
@@ -27,6 +32,7 @@ import cn.com.lasong.utils.ILog;
 public class MuxActivity extends AppBaseActivity {
 
     ActivityMuxBinding binding;
+    ExecutorService executors = Executors.newSingleThreadExecutor();
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,7 +47,6 @@ public class MuxActivity extends AppBaseActivity {
                         if (!isGrant) {
                             return;
                         }
-                        Muxer muxer = new Muxer();
 
                         try {
 //                            File file = new File(Environment.getExternalStorageDirectory(), "geqian.mp3");
@@ -52,7 +57,7 @@ public class MuxActivity extends AppBaseActivity {
                             if (out.exists()) {
                                 out.delete();
                             }
-                            int ret = muxer.remux(file.getAbsolutePath(), out.getAbsolutePath(), 4.8, 0);
+                            int ret = Muxer.remux(file.getAbsolutePath(), out.getAbsolutePath(), 4.8, 0);
                             ILog.d("ret: " + ret);
                             if (ret == 0) {
                                 Intent intent = new Intent(Intent.ACTION_VIEW);
@@ -62,6 +67,85 @@ public class MuxActivity extends AppBaseActivity {
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
+                    }
+                }, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE);
+            }
+        });
+        binding.videoMuxing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestPermissions(new PERCallback() {
+                    @Override
+                    public void onResult(boolean isGrant, Map<String, Boolean> result) {
+                        if (!isGrant) {
+                            return;
+                        }
+                        executors.submit(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    Muxer muxer = new Muxer();
+                                    InputStream is = getAssets().open("rgba_7.raw");
+                                    InputStream is2 = getAssets().open("rgba_8.raw");
+                                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                                    byte[] buffer = new byte[2048];
+                                    int len;
+                                    while ((len = is.read(buffer)) > 0) {
+                                        bos.write(buffer, 0, len);
+                                    }
+                                    byte[] rgba1 = bos.toByteArray();
+                                    bos.reset();
+                                    while ((len = is2.read(buffer)) > 0) {
+                                        bos.write(buffer, 0, len);
+                                    }
+                                    byte[] rgba2 = bos.toByteArray();
+                                    bos.close();
+                                    is.close();
+                                    is2.close();
+                                    File out = new File(Environment.getExternalStorageDirectory(), "muxing.mp4");
+                                    if (out.exists()) {
+                                        out.delete();
+                                    }
+                                    int ret = 0;
+                                    long handle = muxer.init(out.getAbsolutePath());
+                                    ret = muxer.add_video_stream(800_000, 360, 360);
+                                    ret = muxer.scale_video(AVPixelFormat.AV_PIX_FMT_RGBA.ordinal(), 512, 512);
+                                    ret = muxer.start();
+
+                                    int allFrameCount = 30 * 10; // 10s
+                                    int index = 0;
+                                    byte[] rgba = rgba1;
+                                    while (index <= allFrameCount) {
+                                        if (index % 30 == 0) {
+                                            if (rgba == rgba1) {
+                                                rgba = rgba2;
+                                            } else {
+                                                rgba = rgba1;
+                                            }
+                                        }
+                                        ret = muxer.write_video_frame(rgba);
+                                        index++;
+                                    }
+                                    ret = muxer.write_video_frame(null);
+                                    ret = muxer.stop();
+
+                                    ILog.d("ret: " + ret);
+                                    if (ret == 0) {
+                                        runOnUiThread(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                Intent intent = new Intent(Intent.ACTION_VIEW);
+                                                intent.setDataAndType(Uri.parse(out.getAbsolutePath()), "video/mp4");
+                                                startActivity(intent);
+                                            }
+                                        });
+
+                                    }
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
                     }
                 }, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE);
             }
