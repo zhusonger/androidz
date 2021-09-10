dd8ed920d8b44ae1081c502ffb4122fe25b4d62e
zhusong <song.zhu@lasong.com.cn>
2021-09-10 21:03:33 +0800

    更新about

--- zapp/src/main/java/cn/com/lasong/zapp/ui/about/AboutFragment.kt
+++ zapp/src/main/java/cn/com/lasong/zapp/ui/about/AboutFragment.kt
@@ -6,28 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import cn.com.lasong.base.BaseFragment
-import cn.com.lasong.utils.ZCrypto
 import cn.com.lasong.widget.utils.ViewHelper
+import cn.com.lasong.zapp.BuildConfig
+import cn.com.lasong.zapp.R
 import cn.com.lasong.zapp.databinding.FragmentAboutBinding
 
 class AboutFragment : BaseFragment() {
 
     private lateinit var viewModel: AboutViewModel
 
-    private lateinit var binding: FragmentAboutBinding;
+    private lateinit var binding: FragmentAboutBinding
+
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         viewModel = ViewModelProvider(this).get(AboutViewModel::class.java)
         binding = FragmentAboutBinding.inflate(layoutInflater)
+        binding.tvVersion.text = getString(R.string.about_version_name, BuildConfig.VERSION_NAME)
         ViewHelper.setClickAlpha(binding.tvVersion)
-        binding.tvVersion.setOnClickListener {
-            val key = "LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlHZk1BMEdDU3FHU0liM0RRRUJBUVVBQTRHTkFEQ0JpUUtCZ1FDcE1KZWRtL21zdklISnVqOVgwVk41aDJRajFZRnRhZWsKdkpXU1lNUlFhdXVzYzRoM0ljS0I1UDBGZUxnSlpnVUtOWUhVczhXVjdya0VPSVNwaTh1cXphcWZ1M1ZZREdJWgowdEd6bzlwMXZiWGU4bDdXbkQrdTdCVnZ3d2xhcmJpcVIyQXVyMUtxUVVxQkgvZkhBT3RsQ0xLeVZsZnkrVSsxCjFMN3F3QU5yeVBZRlZNVE1sd0lEQVFBQgotLS0tLUVORCBQVUJMSUMgS0VZLS0tLS0K"
-            ZCrypto.validateClientKey(key)
-            ZCrypto.encode("ddd")
-        }
         return binding.root
     }
 }
